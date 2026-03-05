c22c0ad7dcb536994930cb504cea4c4db0c3df1b
cengo1101 <ferhat2000xxxxc@gmail.com>
2026-03-05 20:44:10 +0300

    İlk yükleme

---
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -0,0 +1,115 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Radians;
+
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+
+public class DriveConstants {
+  public static final double maxSpeedMetersPerSec = 4;
+  public static final double odometryFrequency = 100.0; // Hz
+  public static final double trackWidth = Units.inchesToMeters(27.5);
+  public static final double wheelBase = Units.inchesToMeters(27.5);
+  public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
+  public static final Translation2d[] moduleTranslations =
+      new Translation2d[] {
+        new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+      };
+
+  // Zeroed rotation values for each module, see setup instructions
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(-0.95);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.27);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(0);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(-1.91);
+
+  // Device CAN IDs
+  public static final int pigeonCanId = 32;
+
+  public static final int frontLeftDriveCanId = 11;
+  public static final int backLeftDriveCanId = 6;
+  public static final int frontRightDriveCanId = 44;
+  public static final int backRightDriveCanId = 9;
+
+  public static final int frontLeftTurnCanId = 8;
+  public static final int backLeftTurnCanId = 42;
+  public static final int frontRightTurnCanId = 31;
+  public static final int backRightTurnCanId = 40;
+
+  // Drive motor configuration
+  public static final int driveMotorCurrentLimit = 50;
+  public static final double wheelRadiusMeters = Units.inchesToMeters(2);
+  public static final double driveMotorReduction = 6.75; // MAXSwerve with 14 pinion teeth
+  // and 22 spur teeth
+  public static final DCMotor driveGearbox = DCMotor.getNEO(1);
+
+  // Drive encoder configuration
+  public static final double driveEncoderPositionFactor =
+      2 * Math.PI / driveMotorReduction; // Rotor Rotations ->
+  // Wheel Radians
+  public static final double driveEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM
+  // ->
+  // Wheel Rad/Sec
+
+  // Drive PID configuration
+  public static final double driveKp = 0.00048;
+  public static final double driveKd = 0.00000000018;
+  public static final double driveKs = 0.07861;
+  public static final double driveKv = 0.12870;
+  public static final double driveSimP = 0.05;
+  public static final double driveSimD = 0.0;
+  public static final double driveSimKs = 0.0;
+  public static final double driveSimKv = 0.0789;
+
+  // Turn motor configuration
+  public static final boolean turnInverted = true;
+  public static final int turnMotorCurrentLimit = 20;
+  public static final double turnMotorReduction = 21.4285714286;
+  public static final DCMotor turnGearbox = DCMotor.getNEO(1);
+
+  public static final boolean turnEncoderInverted = false;
+  public static final double turnEncoderPositionFactor =
+      2 * Math.PI / turnMotorReduction; // Rotations -> Radians
+  public static final double turnEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / turnMotorReduction; // RPM -> Rad/Sec
+
+  // Turn PID configuration
+  public static final double turnKp = 1.5;
+  public static final double turnKd = 0.0;
+  public static final double turnSimP = 8.0;
+  public static final double turnSimD = 0.0;
+  public static final double turnPIDMinInput = 0; // Radians
+  public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
+
+  // PathPlanner configuration
+  public static final double robotMassKg = 30;
+  public static final double robotMOI = 6.883;
+  public static final double wheelCOF = 1.2;
+  public static final RobotConfig ppConfig =
+      new RobotConfig(
+          robotMassKg,
+          robotMOI,
+          new ModuleConfig(
+              wheelRadiusMeters,
+              maxSpeedMetersPerSec,
+              wheelCOF,
+              driveGearbox.withReduction(driveMotorReduction),
+              driveMotorCurrentLimit,
+              1),
+          moduleTranslations);
+}
